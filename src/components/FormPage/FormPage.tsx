438339163650477dcf9b16ee5a64d6c392615e96
Rob Sassack <rsassack25@gmail.com>
2023-03-19 04:51:43 -0400

    Added content for form page

--- src/components/FormPage/FormPage.tsx
+++ src/components/FormPage/FormPage.tsx
@@ -1,5 +1,31 @@
+import "./FormPage.css";
+
 function FormPage() {
-  return <div className='form'>Form page</div>;
+  return (
+    <div className='form'>
+      <div className='form--option-container'>
+        <h2>Sleep now...</h2>
+        <span>Press this button if you are going to sleep now</span>
+        <button className='form--sleep-now-btn'>Sleep now</button>
+      </div>
+      <div className='form--option-container'>
+        <h2>Wake up at...</h2>
+        <span>Enter the time you want to wake up at</span>
+        <div className='form--input-container'>
+          <input type='time' name='wake-at-input' id='wake-at-input' />
+          <button>Go</button>
+        </div>
+      </div>
+      <div className='form--option-container'>
+        <h2>Sleep at...</h2>
+        <span>Enter the time you will go to sleep</span>
+        <div className='form--input-container'>
+          <input type='time' name='sleep-at-input' id='sleep-at-input' />
+          <button>Go</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FormPage;
